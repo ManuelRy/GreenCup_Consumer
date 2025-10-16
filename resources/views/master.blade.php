d2d01c58ca75a694611f00dfff8b175007b0b5e0
ManuelRy <manuelsokleng99@gmail.com>
2025-10-16 22:49:30 +0700

    export

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Green Cups App</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo/consumer-logo.png') }}">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
